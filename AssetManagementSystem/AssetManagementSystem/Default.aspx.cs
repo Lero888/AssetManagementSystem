d40b96466a5cc4b8b131cbf6da33ba3dd5180945
Lero888 <49852356+Lero888@users.noreply.github.com>
2019-12-05 20:05:22 +0800

    1. Redirect to if Session null added 2. Login as default page

--- AssetManagementSystem/AssetManagementSystem/Default.aspx.cs
+++ AssetManagementSystem/AssetManagementSystem/Default.aspx.cs
@@ -11,7 +11,7 @@ namespace AssetManagementSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Redirect("Login.aspx");            
         }
     }
 }
